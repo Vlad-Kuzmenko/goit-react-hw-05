c1eb9d4098c826e6d15a2b7a6265833337cf0e41
Vlad <vlad.kuzmenko.dev@gmail.com>
2025-06-11 23:56:29 +0300

    Change 404 page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -3,22 +3,6 @@ import s from "./NotFoundPage.module.css";
 import Section from "../../components/Section/Section";
 import Container from "../../components/Container/Container";
 
-// const NotFoundPage = () => {
-//   return (
-//     <Section>
-//       <Container>
-//         <h1 className={s.title}>404</h1>
-//         <p className={s.text}>Not Found</p>
-//         <Link className={s.button} to={"/"}>
-//           Go Home
-//         </Link>
-//       </Container>
-//     </Section>
-//   );
-// };
-
-// export default NotFoundPage;
-
 const NotFound = () => {
   return (
     <Section>
